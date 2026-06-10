b33b40adc1a517f7b273af3c6799778bf678a52a
raphaelcosta93 <raphaelcosta151@yahoo.com>
2026-06-10 09:38:33 -0600

    cloudwatch alarms for ec2

--- main/cloudwatch.tf
+++ main/cloudwatch.tf
@@ -29,8 +29,52 @@ resource "aws_cloudwatch_metric_alarm" "error_alarm" {
   period              = 60
   statistic           = "Sum"
   threshold           = 1
-  alarm_description   = "Fires when an ERROR appears in app logs"
+  alarm_description   = "Fires when an ERROR appears in EC2 app logs"
   alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  tags = {
+    Project   = var.project_name
+    ManagedBy = "terraform"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  alarm_name          = "${var.project_name}-lambda-errors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_description   = "Fires when the image processor Lambda throws an error"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    FunctionName = aws_lambda_function.image_processor.function_name
+  }
+
+  tags = {
+    Project   = var.project_name
+    ManagedBy = "terraform"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "ec2_cpu_high" {
+  alarm_name          = "${var.project_name}-ec2-cpu-high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "Fires when EC2 CPU stays above 80% for 4 minutes"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  dimensions = {
+    InstanceId = aws_instance.web.id
+  }
 
   tags = {
     Project   = var.project_name
